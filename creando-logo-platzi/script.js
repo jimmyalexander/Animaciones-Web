bf54f8430933393fefb2c47a236ccca7d8e636b7
jimmy castiblanco <jkastiblanco@gmail.com>
2019-10-03 23:40:31 -0500

    agregamos actualizacion y ultimos proyectos de clases

---
+++ creando-logo-platzi/script.js
@@ -0,0 +1,23 @@
+/* 
+const $container = document.getElementById('idcontainer')
+
+$container.animate([
+    {
+        transform: 'rotate(0deg) scale(0)',
+        
+    },
+    {
+        
+        transform: 'rotate(980deg) scale(0.7)',
+      
+        
+    }
+],
+{
+    delay: 1000,
+    duration: 1500,
+    direction: 'alternate',
+    iterations: Infinity,
+    easing: 'linear'
+}
+) */
